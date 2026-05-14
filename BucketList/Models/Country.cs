8501acffb6587f898c4fdb9bfbe068469deb73db
Marlies Dens <marlies.dens@pxl.be>
2026-05-14 08:27:13 +0200

    klasse Country en klasse Place

---
+++ BucketList/Models/Country.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace PE_BucketList.Models
+{
+    public class Country
+    {
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private List<Place> _places;
+
+		public List<Place> Places
+		{
+			get { return _places; }
+			set { _places = value; }
+		}
+
+
+
+		public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+    }
+}
